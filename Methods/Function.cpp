6f86c75ee843a763a9f19099d9a5cc2f2e8053dc
Pankaj Chouhan <pankajchouhan80@gmail.com>
2023-09-12 14:24:08 +0000

    update Class Object

---
+++ Methods/Function.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+void getinfo(){
+    cout<<"welcome to codeswithpankaj.com"<<endl;
+}
+
+void getmsg(string msg){
+    cout<<"msg : "<<msg<<endl;
+}
+
+int main(){
+
+    getmsg("Welcome....");
+    getinfo();
+
+
+    return 0;
+}
