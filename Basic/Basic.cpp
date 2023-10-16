657441db1d6545c690f8265914a7a25d7f726f79
Pankaj Chouhan <pankajchouhan80@gmail.com>
2023-10-16 04:23:10 +0000

    Add Basic ..

---
+++ Basic/Basic.cpp
@@ -0,0 +1,11 @@
+#include<iostream> // Header File
+using namespace std; // std lib 
+
+int main(){
+
+    cout<<"welcome to codes with pankaj"<<endl;
+    cout<<"welcome to c++ programming"<<endl;
+    cout<<34<<" this is number"<<endl;
+
+    return 0;
+}
---
+++ Basic/UserInput.cpp
@@ -0,0 +1,22 @@
+#include<iostream> // Header File
+using namespace std; // std lib 
+
+int main(){
+
+    string name;
+    int age;
+    float height;
+
+    cout<<"Enter Your Name -- "<<endl;
+    cin>>name;
+    cout<<"Enter Your Age -- "<<endl;
+    cin>>age;
+    cout<<"Enter Your Height -- "<<endl;
+    cin>>height;
+
+    cout<<"My name is : "<<name<<endl;
+    cout<<"My age is : "<<age<<endl;
+    cout<<"My height is : "<<height<<endl;
+
+    return 0;
+}
