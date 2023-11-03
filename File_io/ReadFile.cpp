fc1d0acf5d7287d8d2f6d64fcf770673f04e48e8
Pankaj Chouhan <pankajchouhan80@gmail.com>
2023-11-03 04:16:57 +0000

    update ..file

--- File_io/ReadFile.cpp
+++ File_io/ReadFile.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 
 int main(){
-    // Create a text string, which is used to output the text file
+// Create a text string, which is used to output the text file
 string myText;
 
 // Read from the text file
 ifstream MyReadFile("p4n.txt");
 
 // Use a while loop together with the getline() function to read the file line by line
-while(getline (MyReadFile, myText)) {
+while (getline (MyReadFile, myText)) {
   // Output the text from the file
   cout << myText;
 }
