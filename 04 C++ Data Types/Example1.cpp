c21886e62cdfce2ec2261371b345dd558f22e178
Pankaj Chouhan <pankajchouhan80@gmail.com>
2024-02-03 04:13:49 +0000

    update example

---
+++ 04 C++ Data Types/Example1.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    // Integer data type
+    int myint = 20;
+
+    cout<<"Int = "<<myint<<endl;
+ 
+    // Floating-point data types
+    double myDouble = 3.14;
+    float myFloat = 2.718f; // 'f' suffix denotes a float
+
+    cout<<"double = "<<myDouble<<endl;
+    cout<<"float = "<<myFloat<<endl;
+
+    // Character data type
+    char myChar = 'A';
+    cout<<"Char = "<<myChar<<endl;
+    // Boolean data type
+    bool myBool = false;
+    cout<<"Bool = "<<myBool<<endl;
+     // String data type from the C++ Standard Library
+     string name = "joy";
+
+    cout<<"String = "<<name<<endl;
+
+    return 0;
+}
---
+++ 05 C++ Variable/Example1.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    const int number = 10;
+
+    cout<<"Number is : "<<number<<endl;
+
+    string name = "joy";
+    int age = 56;
+    float height = 6.7;
+    char ch = 'A';
+
+    cout<<"Your Name is "<<name<<endl;
+    cout<<"Your Age is "<<age<<endl;
+    cout<<"Your height is "<<height<<endl;
+    cout<<"Your ch is "<<ch<<endl;
+
+
+    return 0;
+}
