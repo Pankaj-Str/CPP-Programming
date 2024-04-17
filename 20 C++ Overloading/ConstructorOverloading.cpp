985a04d9b5ed20965d508d27613579cd141cb13a
Pankaj Chouhan <pankajchouhan80@gmail.com>
2024-04-17 09:24:59 +0000

    update overloding

---
+++ 20 C++ Overloading/ConstructorOverloading.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+class info{
+
+
+    public : info(){
+
+        cout<<"welcome to p4n.in"<<endl;
+    
+    }
+
+
+
+};
+
+
+
+int main(){
+
+        // create a object
+
+        info ifo;
+        
+
+        return 0;
+
+}
