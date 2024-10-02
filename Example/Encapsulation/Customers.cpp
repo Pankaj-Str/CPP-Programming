e658ea5d8355b86b779c36eb7c778ee86816e0ee
Pankaj Chouhan <pankajchouhan80@gmail.com>
2024-10-02 02:43:05 +0000

    Encapsulation update example complete

--- Example/Encapsulation/Customers.cpp
+++ Example/Encapsulation/Customers.cpp
@@ -24,16 +24,27 @@ class customer{
         return creditlimit;
     }
 
-    
-
-
-
-
+    public : void setbankname(string BankName){
+        bankname = BankName;
+    }
+    public : string getbankname(){
+        return bankname;
+    }
 };
 
 
 
 int main(){
+
+    customer objcustomer(45000,"SBI");
+    cout<<"Credit Limit : "<<objcustomer.getcreditlimit()<<endl;
+    cout<<"Bank Name : "<<objcustomer.getbankname()<<endl;
+
+    objcustomer.setbankname("HDFC");
+    objcustomer.setcreditlimit(50000);
+    cout<<"Credit Limit : "<<objcustomer.getcreditlimit()<<endl;
+    cout<<"Bank Name : "<<objcustomer.getbankname()<<endl;
+    
 
     return 0;
 }
