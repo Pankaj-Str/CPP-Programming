5ed993224fa11978dbdef1d44e5e05c4d994d190
Pankaj Chouhan <pankajchouhan80@gmail.com>
2024-10-18 09:49:17 +0000

    update Variable topic

--- Example/basic_Example/UserInput.cpp
+++ Example/basic_Example/UserInput.cpp
@@ -11,11 +11,18 @@ int main(){
     string name;
     bool status;
     char gender;
-    name =  "John";
-    age  = 25;
-    height =  4.75;
-    status = true;
-    gender = 'M';
+    
+   cout<<"Enter your name: ";
+   cin>>name;
+   cout<<"Enter your age: ";
+   cin>>age;
+   cout<<"Enter your height: ";
+   cin>>height;
+   cout<<"Enter your gender: ";
+   cin>>gender;
+   cout<<"Enter your status: ";
+   cin>> status;
+
     cout<<"Your Name is : "<<name<<endl;
     cout<<"Your Age is : "<<age<<endl;
     cout<<"Your Height is : "<<height<<endl;
