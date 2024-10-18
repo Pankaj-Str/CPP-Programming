612f972ca5c34a8bd2c179e0763b806445cab1cc
Pankaj Chouhan <pankajchouhan80@gmail.com>
2024-10-18 09:45:10 +0000

    update basic example

---
+++ Example/basic_Example/Basic.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    cout<<"Welcome to codeswithpankaj.com"<<endl;
+    cout<<45456<<endl;
+
+
+    return 0;
+}
+
+
---
+++ Example/basic_Example/Variable.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+
+using  namespace std;
+
+int main(){
+
+
+    int age;
+    float height;
+    string name;
+    bool status;
+    char gender;
+    name =  "John";
+    age  = 25;
+    height =  4.75;
+    cout<<"Your Name is : "<<name<<endl;
+
+
+
+    return 0;
+}
