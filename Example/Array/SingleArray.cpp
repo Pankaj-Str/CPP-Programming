c0e54ed9874324276d51aca2eeea1db5e4b89804
Pankaj Chouhan <pankajchouhan80@gmail.com>
2024-12-18 09:59:32 +0000

    sinlge array update example

---
+++ Example/Array/SingleArray.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main(){
+
+    int data[4];
+
+    data[0] = 27;
+    data[1] = 26;
+    data[2] = 25;
+    data[3] = 24;
+
+    //cout<<"Element on array "<<data[2]<<endl;
+
+    //fatch all data from array 
+
+        for(int i = 0 ; i <4 ;i++){
+            cout<<"Element on array "<<data[i]<<endl;
+        }
+
+    return 0;
+}
