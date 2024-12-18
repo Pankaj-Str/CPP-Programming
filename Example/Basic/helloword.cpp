acb0d18e7bc9d19fbfc12caed4211dd2876e93a3
Pankaj Chouhan <pankajchouhan80@gmail.com>
2024-12-18 09:39:56 +0000

    update basic word example

---
+++ Example/Basic/helloword.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    cout<<"welcome to codeswithpankaj.com"<<endl;
+    cout<<23435<<endl;
+    cout<<"welcome : "<<75675<<endl;
+
+
+    return 0;
+}
