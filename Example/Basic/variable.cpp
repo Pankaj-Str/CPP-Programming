af02676ae679c19f38b9278a4d803ecfcd39ff78
Pankaj Chouhan <pankajchouhan80@gmail.com>
2024-12-18 09:43:12 +0000

    update variable example

--- Example/Basic/variable.cpp
+++ Example/Basic/variable.cpp
@@ -5,7 +5,17 @@ using namespace std;
 int main(){
 
     
-    
+    string name = "joy";
+    int age = 23;
+    float height = 5.5;
+    char gender = 'M';
+
+    cout<<"your name is "<<name<<endl;
+    cout<<"your age is "<<age<<endl;
+    cout<<"your height is "<<height<<endl;
+    cout<<"your gender is "<<gender<<endl;
+
+
 
 
     return 0;
