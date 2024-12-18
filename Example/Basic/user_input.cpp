126406f8a0e1b2d5dee78b8ba46cb34ec429d84d
Pankaj Chouhan <pankajchouhan80@gmail.com>
2024-12-18 09:47:09 +0000

    update user input  in c++

---
+++ Example/Basic/user_input.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main(){
+
+    
+    string name;
+    int age;
+    float height;
+    char gender;
+
+    cout<<"Enter Your Name "<<endl;
+    cin>>name;
+    cout<<"Enter Your Age "<<endl;
+    cin>>age;
+    cout<<"Enter Your Height "<<endl;
+    cin>>height;
+    cout<<"Enter Your Gender "<<endl;
+    cin>>gender;
+
+    cout<<"your name is "<<name<<endl;
+    cout<<"your age is "<<age<<endl;
+    cout<<"your height is "<<height<<endl;
+    cout<<"your gender is "<<gender<<endl;
+
+
+
+
+    return 0;
+}
