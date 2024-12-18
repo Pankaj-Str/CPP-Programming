dac3986b926ed50a0ab6ab8e47b2da214d77d059
Pankaj Chouhan <pankajchouhan80@gmail.com>
2024-12-18 09:51:09 +0000

    update if else

---
+++ Example/if_else_/if_else.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    // smallest number
+
+    int num1 = 10;
+    int num2 = 2;
+
+    if(num1 < num2){
+        cout << "Smallest number is " << num1 << endl;
+    }else{
+        cout << "Smallest number is " << num2 << endl;
+    }
+
+
+
+    return 0;
+}
