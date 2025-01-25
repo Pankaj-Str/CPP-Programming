4d4db8170f2e264b327cdc30deb34e256f34253f
Pankaj Chouhan <pankajchouhan80@gmail.com>
2025-01-25 14:34:35 +0530

    add UserInput program to capture and display user details

--- 05 C++ Basic Input/UserInput.cpp
+++ 05 C++ Basic Input/UserInput.cpp
@@ -1,2 +1,20 @@
 #include<iostream>
 
+using namespace std;
+
+
+int main(){
+
+    string name;
+    int age;
+    float height;
+    cout << "Enter your name: ";
+    cin >> name;
+
+    cout << "Enter your age: ";
+    cin >> age;
+    cout << "Enter your height: ";
+    cin >> height;
+    cout << "Hello, " << name << " you are " << age << " years old and " << height << " feet tall." << endl;
+
+}
