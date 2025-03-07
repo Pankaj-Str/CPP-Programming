a7a0de984129664f10b1413049545846ecf01363
Pankaj Chouhan <pankajchouhan80@gmail.com>
2025-03-07 16:44:43 +0000

    update while loop example in Question_a_11 to clarify functionality and output

--- Questions/Question_a_11.cpp
+++ Questions/Question_a_11.cpp
@@ -1,23 +1,22 @@
 #include<iostream>
-
 using namespace std;
-
 int main(){
-
     // while loop example 
     // start point (initialization)
     // end point (condition)
     // output (print statement)
     // increment ++ or decrement -- (update)
-
+    // ---------Explanation---------
+    // the loop starts with i = 1
+    // it prints of i and increments it in ech iteration
+    // the loop stops when i is greater than 10
+    // -----------------------------
 int i = 1; // initialization
 
     while(i <= 10){ // condition
-        cout << i*2 << endl; // output
+        cout << i << endl; // output
         i++; // update
 
     }
-
     return 0;
-
 }
