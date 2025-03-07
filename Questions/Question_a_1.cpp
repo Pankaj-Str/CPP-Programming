98b7778c84c8a3c9543a72688a76ea47d5b0b27c
Pankaj Chouhan <pankajchouhan80@gmail.com>
2025-03-07 16:41:03 +0000

    add examples of while loop in Question_a_1 and Question_a_11

---
+++ Questions/Question_a_1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    // while loop example 
+    // start point (initialization)
+    // end point (condition)
+    // output (print statement)
+    // increment ++ or decrement -- (update)
+
+
+    int i = 1; // initialization
+
+    while(i <= 10){ // condition
+        cout << i << endl; // output
+        i++; // update
+
+    }
+
+    return 0;
+
+}
---
+++ Questions/Question_a_11.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    // while loop example 
+    // start point (initialization)
+    // end point (condition)
+    // output (print statement)
+    // increment ++ or decrement -- (update)
+
+int i = 1; // initialization
+
+    while(i <= 10){ // condition
+        cout << i*2 << endl; // output
+        i++; // update
+
+    }
+
+    return 0;
+
+}
