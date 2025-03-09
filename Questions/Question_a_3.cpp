80244b1609cb912a6a9ea5f3c02dcf15dd720339
Pankaj Chouhan <pankajchouhan80@gmail.com>
2025-03-09 14:20:59 +0530

    add program to find the maximum of two numbers

---
+++ Questions/Question_a_3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 10;
+    int b = 7;
+        
+    // find the maximum of two numbers
+    //a > b ? cout << a : cout << b;
+    // condition ? true : false
+
+    a > b  ? cout << "a is greater" : cout << "b is greater";
+
+    return 0;
+}
