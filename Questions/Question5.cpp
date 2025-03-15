74966c702e5e1165e6986de2e81fac7608a4cdbc
Pankaj Chouhan <pankajchouhan80@gmail.com>
2025-03-15 22:27:10 +0530

    add program to demonstrate input of four numbers in C++

---
+++ Questions/Question5.cpp
@@ -0,0 +1,26 @@
+#include<iostream> 
+//#include <iomanip.h> 
+//#include <conio.h> 
+using namespace std;
+int  main()
+//header file for input/output //header file for setprecision function
+{
+//clrscr();
+float a,b,c,d;
+cout<<"Enter any four numbers."<<endl;
+cout<<"1st number: ";
+cin>>a;
+cout<<"2nd number: "; 
+cin>>b;
+cout<<"3rd number: "; 
+cin>>c;
+cout<<"4th number: "; 
+cin>>d;
+
+
+
+ //end of outer else part
+//getch();
+return 0;
+}
+//end of program
