0f38e90613a29b795eb74fa5601dda557d8ca5f2
Pankaj Chouhan <pankajchouhan80@gmail.com>
2025-06-03 14:37:29 +0530

    Refactor code structure for improved readability and maintainability

---
+++ 10 C++ Functions/Function.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+
+class student{
+
+    public : void info(){
+        cout<<"welcome to codeswithpankaj.com"<<endl;
+    }
+
+
+};
+
+int main(){
+
+    student obj;
+    obj.info();
+
+
+
+    return 0;
+}
